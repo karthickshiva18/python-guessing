1f986d95391b55037f4f60439c4ba2a0c9d4c096
karthick <karthickshekar@live.com>
2019-12-02 16:29:56 +0530

    guessing

---
+++ guess.py
@@ -0,0 +1,16 @@
+import random
+n = 100
+to_be_guessed = int(n * random.random()) + 1
+guess = 0
+while guess != to_be_guessed:
+    guess = int(input("new number: "))
+    if guess > 0:
+        if guess > to_be_guessed:
+            print("Number is too large")
+        elif guess < to_be_guessed:
+            print("Number is too small")
+    else:
+        print("sorry not correct")
+        break
+else:
+    print("congratulation you made it")
